23d31ad42c687f90c8f515a702a434d691b5ddfe
Altuğ Bakan <mail@alt.ug>
2023-08-11 22:01:07 +0200

    Update vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["react", "react-router", "react-router-dom"],
       output: {
         globals: {
           react: "React",
